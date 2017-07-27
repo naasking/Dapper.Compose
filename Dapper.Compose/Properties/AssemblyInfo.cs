4a1711bcddc083c73254e2568388f70ab720bc38
Sandro Magi <naasking@gmail.com>
2017-07-27 15:34:08 -0400

    updated version number for next release

--- Dapper.Compose/Properties/AssemblyInfo.cs
+++ Dapper.Compose/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-RC6")]
+[assembly: AssemblyInformationalVersion("1.0.0-RC7")]
 [assembly: AssemblyFileVersion("1.0.0")]
